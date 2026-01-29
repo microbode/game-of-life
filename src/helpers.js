55649ea211fa006677db36b3057578eb81446f49
David Vázquez <david.vazquez.alonso@gmail.com>
2026-01-29 01:43:32 +0100

    Change from top left corner to center the cell used to apply pattern in board
    Signed-off-by: David Vázquez <david.vazquez.alonso@gmail.com>

--- src/helpers.js
+++ src/helpers.js
@@ -6,13 +6,18 @@ export const applyPattern = ({ pattern, state, initialX, initialY }) => {
   const columnsNumber = state.length;
   const rowsNumber = state[0].length;
   const newState = state.map((column) => [...column]);
+
+  // Center the pattern on the click position
+  const offsetX = Math.floor(pattern.length / 2);
+  const offsetY = Math.floor(pattern[0].length / 2);
+
   pattern.forEach((column, x) =>
     column.forEach(
       (cell, y) =>
-        (newState[(initialX + x + columnsNumber) % columnsNumber][
-          (initialY + y + rowsNumber) % rowsNumber
-        ] = cell)
-    )
+        (newState[(initialX - offsetX + x + columnsNumber) % columnsNumber][
+          (initialY - offsetY + y + rowsNumber) % rowsNumber
+        ] = cell),
+    ),
   );
   return newState;
 };
